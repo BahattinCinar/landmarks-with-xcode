c6535582d194a572f9bdabea16c77f9cf3787e9c
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2024-02-10 23:36:27 +0300

    Add files via upload

---
+++ landmarks/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  landmarks
+//
+//  Created by Behaüddin Çınar on 9.02.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView : View{
+    var body: some View{
+        VStack{
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello World")
+        }
+        .padding()
+    }
+}
+
+#Preview{
+    ContentView()
+}
